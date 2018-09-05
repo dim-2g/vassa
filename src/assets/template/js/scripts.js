2011989623117bf2d44a77d476e1d44f8408b161
dim-2g <dim-2g@yandex.ru>
2018-09-06 02:39:37 +0300

    почти все

--- src/assets/template/js/scripts.js
+++ src/assets/template/js/scripts.js
@@ -19,9 +19,41 @@ $(function() {
     })
     //$('[data-mask]').inputmask({"mask": $(this).attr('data-mask')});
 
-
+    initPeopleSlider();
+    initPhotoSlider();
 
 });
+
+var slider_people = false;
+initPeopleSlider  = function() {
+    if (!slider_people) {
+        $('.people-slider').slick({
+            'autoplay': false,
+            'arrows': true,
+            'dots': true,
+            'slidesToShow': 2,
+            'slidesToScroll': 1,
+            'adaptiveHeight': true
+        });
+        slider_people = true;
+    }
+};
+
+var slider_photo = false;
+initPhotoSlider  = function() {
+    if (!slider_photo) {
+        $('.photo-slider').slick({
+            'autoplay': false,
+            'arrows': false,
+            'dots': true,
+            'slidesToShow': 6,
+            'slidesToScroll': 1,
+            'adaptiveHeight': true
+        });
+        slider_photo = true;
+    }
+};
+
 $(window).resize(function(){
     var width = $(window).width();
     //collapseReview(1);
